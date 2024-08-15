be4f2fd188705966654d4eece3905f546f6e2cea
maltelueken <malte_lueken@arcor.de>
2024-08-15 14:18:00 +0200

    Refactor RDM likelihood function

--- src/pymc_models.py
+++ src/pymc_models.py
@@ -14,49 +14,47 @@ from pymc.distributions.shape_utils import rv_size_is_none
 from pymc.distributions.transforms import _default_transform
 
 
-def inv_gauss_pdf(t, mu, lam):
+def inv_gauss_logpdf(t, mu, lam):
 
     e = -(lam/(2*t)) * (t**2/mu**2 - 2*t/mu  + 1)
 
-    x = pt.exp(e + .5*pt.log(lam) - .5*pt.log(2*t**3*pt.pi))
-    x = pt.maximum(0, x)
+    x = e + .5*pt.log(lam) - .5*pt.log(2*t**3*pt.pi)
 
     return x
 
 
-def standard_normal_cdf(t):
-    return 0.5 * (1 + pt.erf(t/pt.sqrt(2)))
+def standard_normal_logcdf(t):
+    return pt.log(0.5 * (1 + pt.erf(t/pt.sqrt(2))))
 
 
-def inv_gauss_cdf(t, mu, lam):
-
-    e = pt.exp(pt.log(2*lam) - pt.log(mu))
-    add = pt.sqrt(lam/t) * (1 + t/mu)
-    sub = pt.sqrt(lam/t) * (1 - t/mu)
-
-    p_1 = 1 - standard_normal_cdf(add)
-    p_2 = 1 - standard_normal_cdf(sub)
-    x = pt.exp(e + pt.log(p_1)) + p_2
-    x = pt.maximum(0, x)
-    return x
+def inv_gauss_logsf(t, mu, lam):
+    """https://journal.r-project.org/archive/2016-1/giner-smyth.pdf"""
+    lam = 1/lam
+    qm = t/mu
+    lamm = lam*mu
+    r = pt.sqrt(t*lam)
+    a = standard_normal_logcdf(-(qm - 1.0)/r)
+    b = 2.0/lamm + standard_normal_logcdf(-(qm + 1.0)/r)
+    return a + pt.log1p(-pt.exp(b - a))
 
 
 class RdmSimpleRV(RandomVariable):
     name = "wald"
-    signature = "(),(),(),(),()->(2)"
+    signature = "(),(),(),(),(),()->(2)"
     dtype = "floatX"
     _print_name = ("Wald", "\\operatorname{Wald}")
 
     @classmethod
-    def rng_fn(cls, rng, drift_winner, drift_loser, s_winner, threshold, ndt, size) -> np.ndarray:
+    def rng_fn(cls, rng, drift_winner, drift_loser, s_winner, s_loser, threshold, ndt, size) -> np.ndarray:
         v = np.hstack([drift_loser, drift_winner])
+        s = np.hstack([s_loser, s_winner])
 
         mu = threshold / v
-        lam = (threshold / s_winner) ** 2
+        lam = (threshold / s) ** 2
 
         # First passage time
-        fpt_winner = rng.wald(mu[1], lam, size=size)
-        fpt_loser = rng.wald(mu[0], lam, size=size)
+        fpt_winner = rng.wald(mu[1], lam[1], size=size)
+        fpt_loser = rng.wald(mu[0], lam[0], size=size)
 
         fpt = np.c_[fpt_loser, fpt_winner]
 
@@ -77,19 +75,23 @@ class RdmSimple(Continuous):
         cls,
         drift_winner: DIST_PARAMETER_TYPES | None = None,
         drift_loser: DIST_PARAMETER_TYPES | None = None,
+        s_winner: DIST_PARAMETER_TYPES | None = None,
+        s_loser: DIST_PARAMETER_TYPES | None = None,
         threshold: DIST_PARAMETER_TYPES | None = None,
         ndt: DIST_PARAMETER_TYPES | None = None,
         **kwargs,
     ):
         drift_winner = pt.as_tensor_variable(drift_winner)
         drift_loser = pt.as_tensor_variable(drift_loser)
+        s_winner = pt.as_tensor_variable(s_winner)
+        s_loser = pt.as_tensor_variable(s_loser)
         threshold = pt.as_tensor_variable(threshold)
         ndt = pt.as_tensor_variable(ndt)
-        return super().dist([drift_winner, drift_loser, threshold, ndt], **kwargs)
+        return super().dist([drift_winner, drift_loser, s_winner, s_loser, threshold, ndt], **kwargs)
 
 
-    def support_point(rv, size, drift_winner, drift_loser, threshold, ndt):
-        drift_winner, _, _, _ = pt.broadcast_arrays(drift_winner, drift_loser, threshold, ndt)
+    def support_point(rv, size, drift_winner, drift_loser, s_winner, s_loser, threshold, ndt):
+        drift_winner, _, _, _, _, _ = pt.broadcast_arrays(drift_winner, drift_loser, s_winner, s_loser, threshold, ndt)
         if not rv_size_is_none(size):
             drift_winner = pt.full(size, drift_winner)
         return drift_winner
@@ -104,11 +106,9 @@ class RdmSimple(Continuous):
         lam_winner = (threshold/s_winner)**2
         lam_loser = (threshold/s_loser)**2
 
-        f = pt.log(inv_gauss_pdf(value, mu_winner, lam_winner))
+        f = inv_gauss_logpdf(value, mu_winner, lam_winner)
 
-        F = inv_gauss_cdf(value, mu_loser, lam_loser)
-
-        S = pt.log(1. - F)
+        S = inv_gauss_logsf(value, mu_loser, lam_loser)
 
         logp = f + S
 
@@ -118,6 +118,8 @@ class RdmSimple(Continuous):
             logp,
             drift_winner > 0,
             drift_loser > 0,
+            s_winner > 0,
+            s_loser > 0, 
             threshold > 0,
             ndt > 0,
             msg="drift_winner > 0, drift_loser > 0, threshold > 0, ndt > 0",
@@ -151,6 +153,6 @@ def rdm_model_simple(sim_data, **kwargs):
         RdmSimple("ll_true", drift_winner=v_true, drift_loser=v_false, s_winner=s_true, s_loser=1.0, threshold=b, ndt=t0, observed=rt_true)
         RdmSimple("ll_false", drift_winner=v_false, drift_loser=v_true, s_winner=1.0, s_loser=s_true, threshold=b, ndt=t0, observed=rt_false)
 
-        trace = pm.sample(step=pm.step_methods.HamiltonianMC(), initvals={"v_intercept": 2, "v_slope": 1, "a": 2, "s_true": 1, "t0": 0.2}, **kwargs)
+        trace = pm.sample(step=pm.step_methods.HamiltonianMC(), initvals={"v_intercept": 2, "v_slope": 1, "s_true": 1.0, "b": 2, "t0": 0.2}, **kwargs)
 
     return trace

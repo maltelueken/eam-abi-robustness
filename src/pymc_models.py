Fix numerical stability in Wald logcdf

src/pymc_models.py
@@ -22,26 +22,29 @@ from pymc.sampling_jax import get_jaxified_logp
 
 def inv_gauss_logpdf(t, mu, lam):
 
-    e = -(lam/(2*t)) * (t**2/mu**2 - 2*t/mu  + 1)
+    e = -(lam / (2 * t)) * (t**2 / mu**2 - 2 * t / mu  + 1)
 
-    x = e + .5*pt.log(lam) - .5*pt.log(2*t**3*pt.pi)
+    x = e + 0.5 * pt.log(lam) - 0.5 * pt.log(2 * t**3 * pt.pi)
 
     return x
 
 
 def standard_normal_logcdf(t):
-    return pt.log(0.5 * (1 + pt.erf(t/pt.sqrt(2))))
+    return pt.switch(
+        pt.lt(t, -1.0),
+        pt.log(pt.erfcx(-t / pt.sqrt(2.0)) / 2.0) - pt.sqr(t) / 2.0,
+        pt.log1p(-pt.erfc(t / pt.sqrt(2.0)) / 2.0),
+    )
 
 
 def inv_gauss_logsf(t, mu, lam):
     """https://journal.r-project.org/archive/2016-1/giner-smyth.pdf"""
-    lam = 1/lam
-    qm = t/mu
-    lamm = lam*mu
-    r = pt.sqrt(t*lam)
-    a = standard_normal_logcdf(-(qm - 1.0)/r)
-    b = 2.0/lamm + standard_normal_logcdf(-(qm + 1.0)/r)
-    return a + pt.log1p(-pt.exp(b - a))
+    mu = mu / lam
+    t = t / lam
+    r = 1.0 / pt.sqrt(t)
+    a = standard_normal_logcdf(-r * ((t / mu) - 1.0))
+    b = 2.0 / mu + standard_normal_logcdf(-r * (t + mu) / mu)
+    return pt.switch(pt.isposinf(t), -np.inf, pt.switch(pt.gt(t, 0.0), a + pt.log1p(-pt.exp(b - a)), 0.0))
 
 
 class RdmSimpleRV(RandomVariable):

tests/test_pymc_models.py
@@ -4,12 +4,15 @@ import pytest
 
 import scipy.stats as stats
 
-from pymc_models import inv_gauss_logpdf, inv_gauss_logsf
+from pymc_models import inv_gauss_logpdf, inv_gauss_logsf, standard_normal_logcdf
 
 
-def test_inv_gauss_logpdf():
-    x = np.array([-1, 0, 0.01, 0.5, 1.0, 2.0, 10.0, 100.0, 500.0, np.inf, -np.inf])
+@pytest.fixture()
+def x():
+    return np.array([-1, 0, 0.01, 0.5, 1.0, 2.0, 10.0, 100.0, 500.0, np.inf, -np.inf])
 
+
+def test_inv_gauss_logpdf(x):
     mu = np.array([0.01, 1.0, 2.0, 10.0, 100.0])
     lam = np.array([0.01, 1.0, 2.0, 10.0, 100.0])
 
@@ -18,19 +21,24 @@ def test_inv_gauss_logpdf():
             ref = stats.invgauss.logpdf(x, mu=m/l, scale=l)
             res = inv_gauss_logpdf(x, m, l).eval()
 
-            assert pytest.approx(res[np.isfinite(res)] == ref[np.isfinite(ref)])
+            assert np.all(pytest.approx(res[np.isfinite(res)]) == ref[np.isfinite(ref)])
+
+
+def test_standard_normal_logcdf(x):
+    ref = stats.norm.logcdf(x)
+    res = standard_normal_logcdf(x).eval()
+    assert np.all(pytest.approx(res) == ref)
 
 
 # @pytest.mark.xfail()
-def test_inv_gauss_logsf():
-    x = np.array([-1, 0, 0.01, 0.5, 1.0, 2.0, 10.0, 100.0, 500.0, np.inf, -np.inf])
+def test_inv_gauss_logsf(x):
 
     mu = np.array([0.01, 1.0, 2.0, 10.0, 100.0])
     lam = np.array([0.01, 1.0, 2.0, 10.0, 100.0])
 
     for m in mu:
         for l in lam:
-            ref = stats.invgauss.logsf(x, mu=2/2, scale=2)
-            res = inv_gauss_logsf(x, 2, 2).eval()
+            ref = stats.invgauss.logsf(x, mu=m/l, scale=l)
+            res = inv_gauss_logsf(x, m, l).eval()
 
-            assert np.all(res == ref)
+            assert np.all(pytest.approx(ref) == res)
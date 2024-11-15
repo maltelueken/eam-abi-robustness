ae8f7ac3fc9e19c7e58b8a96516bd5dc9338e5c9
maltelueken <malte_lueken@arcor.de>
2024-11-15 17:37:08 +0100

    Update to latest bayesflow version commit 994916c

--- experiment_1/check_prior_pushforward_rdmc.py
+++ experiment_1/check_prior_pushforward_rdmc.py
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 def prior_pushforward(cfg: DictConfig):
     simulator = instantiate(cfg["simulator"])
 
-    param_names = cfg["approximator"]["data_adapter"]["inference_variables"]
+    param_names = cfg["approximator"]["adapter"]["inference_variables"]
     
     forward_dict = simulator.sample((500,), num_obs=np.tile(np.array([500]), (500,)))
 
@@ -23,7 +23,7 @@ def prior_pushforward(cfg: DictConfig):
     fig = create_prior_2d_plot(prior_samples, param_names)
     fig.savefig("prior2d.png")
 
-    data = forward_dict[cfg["approximator"]["data_adapter"]["summary_variables"][0]]
+    data = forward_dict[cfg["approximator"]["adapter"]["summary_variables"][0]]
 
     fig = create_pushforward_plot_rdmc(data[:25, :, :], prior_samples, param_names)
     fig.savefig("prior_pushforward.png")

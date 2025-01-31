255ed981adbdb8b841e2bf16513d0f8f9189342f
maltelueken <malte_lueken@arcor.de>
2025-01-31 17:00:12 +0100

    Add MMD robustness computation

--- experiment_1/check_robustness.py
+++ experiment_1/check_robustness.py
@@ -1,14 +1,19 @@
 import logging
 import os
 
+if "KERAS_BACKEND" not in os.environ:
+    # set this to "torch", "tensorflow", or "jax"
+    os.environ["KERAS_BACKEND"] = "jax"
+
 import bayesflow as bf
 import blackjax
 import hydra
 import numpy as np
+import pandas as pd
 from hydra.utils import get_object, instantiate
 from omegaconf import DictConfig
 
-from data import load_hdf5, save_hdf5
+from data import load_hdf5
 from utils import create_missing_dirs, create_robustness_2d_plot, convert_prior_samples, convert_posterior_samples
 
 logger = logging.getLogger(__name__)
@@ -21,6 +26,8 @@ def check_robustness(cfg: DictConfig):
 
     create_missing_dirs(["robustness"])
 
+    mmd = []
+
     for t in sample_sizes:
         test_data_path = os.path.join(cfg["test_data_path"], "test_data", f"test_data_sample_size_{t}.hdf5")
         logger.info("Loading test data from %s", os.path.abspath(test_data_path))
@@ -32,8 +39,9 @@ def check_robustness(cfg: DictConfig):
 
         posterior_mcmc = mcmc_samples["samples"]
         is_converged = np.all(blackjax.diagnostics.potential_scale_reduction(posterior_mcmc, chain_axis=1, sample_axis=2) < 1.01, axis=1)
+        
+        logger.info("%s MCMC models did not converge: %s", 1.0-is_converged.mean(), np.where(~is_converged))
         posterior_mcmc = np.exp(np.reshape(posterior_mcmc, (posterior_mcmc.shape[0], -1, posterior_mcmc.shape[3])))[is_converged]
-        # posterior_mcmc = posterior_mcmc[:, :, [3, 2, 4, 0, 1]]
 
         prior_samples = convert_prior_samples(forward_dict, param_names)
 
@@ -47,7 +55,12 @@ def check_robustness(cfg: DictConfig):
         fig = create_robustness_2d_plot(posterior_npe[idx,:,:], posterior_mcmc[idx,:,:], prior_samples, prior_samples[idx,:], param_names)
         fig.savefig(os.path.join("robustness", f"robustness_2d_sample_size_{t}.png"))
 
-        logger.info("%s datasets did not converge: %s", 1.0-is_converged.mean(), np.where(~is_converged))
+        mmd.append(np.array([bf.metrics.functional.maximum_mean_discrepancy(x, y) for x, y in zip(posterior_mcmc, posterior_npe)]))
+
+    pd.DataFrame({
+        "sample_size": sample_sizes,
+        "mmd": mmd
+    }).explode("mmd").to_csv(os.path.join("robustness", "mmd.csv"))
 
 
 if __name__ == "__main__":

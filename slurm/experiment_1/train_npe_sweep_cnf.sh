ecffee9635a2f7be427c75aef31d2e857bb8bcde
maltelueken <malte_lueken@arcor.de>
2025-03-21 14:40:41 +0100

    Add slurm script for NPE sweep with coupling flows

---
+++ slurm/experiment_1/train_npe_sweep_cnf.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=train_npe
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=9
+#SBATCH --gpus=1
+#SBATCH --partition=gpu_mig
+#SBATCH --time=120:00:00
+#SBATCH --output=/projects/0/prjs1372/eam-abi-robustness/slurm/logs/train_npe_%j.out
+#SBATCH --error=/projects/0/prjs1372/eam-abi-robustness/slurm/logs/train_npe_%j.err
+
+cd /projects/0/prjs1372/eam-abi-robustness
+
+module load 2023
+
+source bin/activate
+
+python experiment_1/train_npe.py \
+    --multirun \
+    sweeper=optuna \
+    inference-method=coupling_flow

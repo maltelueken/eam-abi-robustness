d1dc65db30cea2bae22bdc2817a869f816d41081
maltelueken <malte_lueken@arcor.de>
2025-03-21 09:20:51 +0100

    Update NPE prediction slurm parameters

--- slurm/experiment_1/predict_npe.sh
+++ slurm/experiment_1/predict_npe.sh
@@ -9,15 +9,21 @@
 #SBATCH --output=/projects/0/prjs1372/eam-abi-robustness/slurm/logs/predict_npe_%j.out
 #SBATCH --error=/projects/0/prjs1372/eam-abi-robustness/slurm/logs/predict_npe_%j.err
 
+cd /projects/0/prjs1372/eam-abi-robustness
+
+module load 2023
+
+source bin/activate
+
 python experiment_1/predict_npe.py \
     --multirun \
-    embed_depth=3 \
-    embed_width=7 \
-    inference_mlp_width=9 \
-    mlp_depth=1 \
-    mlp_width=8 \
     sweeper=basic \
-    workflow.inference_network.subnet_kwargs.depth=9 \
-    workflow.inference_network.use_optimal_transport=True \
-    workflow.summary_network.num_seeds=2 \
-    workflow.summary_network.summary_dim=14
+    inference_mlp_depth=5 \
+    inference_mlp_width=8 \
+    approximator.inference_network.use_optimal_transport=true \
+    approximator.summary_network.summary_dim=19 \
+    embed_depth=1 \
+    embed_width=4 \
+    mlp_depth=2 \
+    mlp_width=5 \
+    approximator.summary_network.num_seeds=3

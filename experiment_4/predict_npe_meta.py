1481f3fb48865a48721bb3a90dcd5d209651a8eb
maltelueken <malte_lueken@arcor.de>
2025-08-22 17:12:29 +0200

    Add NPE prediction for experiment 4

--- experiment_4/predict_npe_meta.py
+++ experiment_4/predict_npe_meta.py
@@ -8,6 +8,9 @@ if "KERAS_BACKEND" not in os.environ:
 # os.environ["JAX_PLATFORMS"] = "cpu"
 
 import hydra
+import numpy as np
+import optuna
+import polars as pl
 from omegaconf import DictConfig
 
 from data import save_hdf5
@@ -22,10 +25,28 @@ def generate_test_data(cfg: DictConfig):
 
     create_missing_dirs(["npe_samples"])
 
+    meta_param_name_1 = cfg["meta_param_name_1"]
+    meta_param_name_2 = cfg["meta_param_name_2"]
+
+
     for filename in os.listdir(os.path.join(cfg["test_data_path"], "test_data")):
         logger.info("Loading test data for file %s", filename)
 
+        study = optuna.create_study(
+            study_name=os.path.splitext(filename)[0],
+            storage="sqlite:///check_summary_mmd.db",
+            load_if_exists=True
+        )
+
+        p1 = study.best_params[meta_param_name_1]
+        p2 = study.best_params[meta_param_name_2]
+
+        logger.info("Predicting with parameters %s", (p1, p2))
+
         data = read_data_from_txt(os.path.join(cfg["test_data_path"], "test_data", filename))
+
+        data[meta_param_name_1] = np.array(p1)
+        data[meta_param_name_2] = np.array(p2)
 
         posterior_samples= approximator.sample(
             conditions=data,
@@ -33,6 +54,7 @@ def generate_test_data(cfg: DictConfig):
         )
 
         save_hdf5(os.path.join("npe_samples", f"posterior_samples_{os.path.splitext(filename)[0]}.hdf5"), posterior_samples)
+
 
 
 if __name__ == "__main__":

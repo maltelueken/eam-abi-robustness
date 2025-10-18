b63734e50af056d8c37010727ec8820cabc386dd
maltelueken <malte_lueken@arcor.de>
2025-10-18 14:15:53 +0200

    Add script for downloading results

--- download_metrics.sh
+++ download_metrics.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
+experiment=experiment_1
+
+for model in "rdm_simple" "rdm_simple_discrete_lower" "rdm_simple_discrete_upper" "rdm_simple_discrete_full"; do
+    mkdir -p outputs/$experiment/$model/flow_matching/
+    scp -r snellius:/projects/prjs1372/eam-abi-robustness/outputs/$experiment/$model/flow_matching/metrics/ \
+        outputs/$experiment/$model/flow_matching/
+done
+
+experiment=experiment_2
+
+for model in "rdm_simple" "rdm_simple_lower" "rdm_simple_upper" "rdm_simple_meta" "rdm_simple_meta_lower" "rdm_simple_meta_upper"; do
+    mkdir -p outputs/$experiment/$model/flow_matching/
+    scp -r snellius:/projects/prjs1372/eam-abi-robustness/outputs/$experiment/$model/flow_matching/metrics/ \
+        outputs/$experiment/$model/flow_matching/
+done
+
 experiment=experiment_4
 
-for model in rdm_simple rdm_simple_meta rdm_simple_meta_lower rdm_simple_meta_upper rdm_simple_meta_no_params rdm_simple_meta_lower_no_params rdm_simple_meta_upper_no_params; do
+for model in "rdm_simple" "rdm_simple_lower" "rdm_simple_upper" "rdm_simple_meta" "rdm_simple_meta_lower" "rdm_simple_meta_upper"; do
     mkdir -p outputs/$experiment/$model/flow_matching/
     scp -r snellius:/projects/prjs1372/eam-abi-robustness/outputs/$experiment/$model/flow_matching/metrics/ \
         outputs/$experiment/$model/flow_matching/
